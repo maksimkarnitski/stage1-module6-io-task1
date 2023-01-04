494bf6648880d2f9cf515a57305062d38696f545
Maksim_Karnitski <maksim.kornitsky@gmail.com>
2023-01-04 21:07:33 +0100

    Implement using ducktape

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String fileContent = "";
+        FileInputStream input;
+        try {
+            input = new FileInputStream(file);
+            int ch;
+            while ((ch=input.read()) != -1) {
+                fileContent = fileContent + (char)ch;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(fileContent);
+        String[] abc = fileContent.split("\n");
+
+        String nameValue = abc[0].substring(abc[0].indexOf(" ") + 1, abc[0].indexOf("\r"));
+        int ageValue = Integer.parseInt(abc[1].substring(abc[1].indexOf(" ") + 1, abc[1].indexOf("\r")));
+        String emailValue = abc[2].substring(abc[2].indexOf(" ") + 1, abc[2].indexOf("\r"));
+        long phoneValue = Long.parseLong(abc[3].substring(abc[3].indexOf(" ") + 1, abc[3].indexOf("\r")));
+        return new Profile(nameValue, ageValue, emailValue, phoneValue);
+//        return new Profile();
     }
 }
